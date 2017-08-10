a61c63ca9d78291fd86437ed80126ef11bb765ba
imallarino2015 <imallarino2015@fau.edu>
2017-08-09 20:14:45 -0400

    Code cleanup and refactoring

--- Snake/snake.py
+++ Snake/snake.py
@@ -10,11 +10,11 @@
 
 print("Snake")
 
-from random import *
+from random import *	#for spawning the food
 seed(a=None)
 
 from tkinter import *
-from tkinter import messagebox
+from tkinter import messagebox	#for the end-of-game message boxes
 
 DELAY=50
 WIDTH=640
@@ -24,7 +24,7 @@ KEY_UP='w'
 KEY_DOWN='s'
 KEY_LEFT='a'
 KEY_RIGHT='d'
-KEY_START=' '
+KEY_START=' '	#start is set to the spacebar
 
 class App(Frame):
 	def __init__(self,master=None):
@@ -58,10 +58,8 @@ class App(Frame):
 		self.snake.direction=self.snake.dir	#reduce the buffered inputs to the program's speed
 		self.snake.moveSnake()
 		
-		for a in range(0,len(self.snake.body)-1):	#the snake crossed itself
-			if(self.snake.body[a].isOccupying(self.snake.x,self.snake.y)):
-				self.endGame()
-				break
+		if(self.snake.bodyOccupies(self.snake.x,self.snake.y)):	#the snake crossed itself
+			self.endGame()
 
 		if(self.snake.x<0 or self.snake.x>=RES or
 			self.snake.y<0 or self.snake.y>=RES):	#out of bounds
@@ -145,7 +143,7 @@ class SnakeHead(SnakeSegment):
 	def grow(self):
 		dX=0
 		dY=0
-		if(self.direction=="up"):
+		if(self.direction=="up"):	#TODO: Find some way to clean up
 			dX=0
 			dY=1
 		if(self.direction=="down"):
@@ -165,6 +163,12 @@ class SnakeHead(SnakeSegment):
 				self.body[len(self.body)-1].x+dX,
 				self.body[len(self.body)-1].y+dY)]
 		return
+		
+	def bodyOccupies(self,x,y):
+		for a in range(0,len(self.body)-1):
+			if(self.body[a].isOccupying(x,y)):
+				return True
+		return False
 
 	def moveSnake(self):
 		if(self.direction!="none"):
@@ -174,10 +178,9 @@ class SnakeHead(SnakeSegment):
 						self.body[len(self.body)-1-a-1].x-self.body[len(self.body)-1-a].x,
 						self.body[len(self.body)-1-a-1].y-self.body[len(self.body)-1-a].y
 					)
-				
-			self.body[0].move(self.x-self.body[0].x,self.y-self.body[0].y)
+				self.body[0].move(self.x-self.body[0].x,self.y-self.body[0].y)
 
-		if(self.direction=="up"):
+		if(self.direction=="up"):	#TODO: Find some way to clean up
 			self.move(0,-1)
 			return
 		if(self.direction=="down"):
@@ -191,7 +194,7 @@ class SnakeHead(SnakeSegment):
 			return
 
 	def keyPress(event):
-		if(event.char==KEY_UP and app.snake.direction!="down"):
+		if(event.char==KEY_UP and app.snake.direction!="down"):	#TODO: Find some way to clean up
 			app.snake.dir="up"
 		if(event.char==KEY_DOWN and app.snake.direction!="up"):
 			app.snake.dir="down"
@@ -202,12 +205,9 @@ class SnakeHead(SnakeSegment):
 
 class Food(Cell):
 	def __init__(self,snake,canvas):
-		self.x=0
-		self.y=0
+		(self.x,self.y)=self.getCoords(snake)
 		self.canvas=canvas
-		self.consumption=0
 		super(Food, self).__init__(canvas,self.x,self.y)
-		self.reset(snake)
 		self.color="green"
 		canvas.itemconfig(self.rect,fill=self.color)
 		return
@@ -231,10 +231,8 @@ class Food(Cell):
 			
 			if(snake.isOccupying(x,y)):
 				unoccupied=False
-			
-			for a in range(0,len(snake.body)):
-				if(snake.body[a].isOccupying(x,y)):
-					unoccupied=False
+			if(snake.bodyOccupies(x,y)):
+				unoccupied=False
 					
 			if(unoccupied==True):
 				return (x,y)
